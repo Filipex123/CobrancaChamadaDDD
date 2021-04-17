71e3bfd63b4e90a13d2e9d30c45005562d3a7cfe
Filipe Zanelato <filipezanelato23@gmail.com>
2021-04-17 19:07:18 -0300

    adicao de erro no looping e palavra chave de saida

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,30 +11,37 @@ public class Main {
 
     public static void main(String[] args) {
 
-        try {
-            ListaDDDs listaDDDs = JsonUtils.getListaDDD();
+        Fila filaChamada = new Fila();
+        ListaDDDs listaDDDs = populaListaDDD(filaChamada);
 
-            Fila filaChamada = new Fila();
-
-            filaChamada.insere(new Chamada("9900000000", listaDDDs));
-            filaChamada.insere(new Chamada("34111111111", listaDDDs));
-            filaChamada.insere(new Chamada("3422222-2222", listaDDDs));
-            filaChamada.insere(new Chamada("(34)333333333", listaDDDs));
-            filaChamada.insere(new Chamada("(11)444444444", listaDDDs));
-
-            while (true) {
-                System.out.println("#### Para encerrar do programa digite 'sair' ####");
-                System.out.println("Digite seu numero: ");
+        while (true) {
+            System.out.println("#### Para encerrar do programa digite 'sair' ####");
+            System.out.println("Digite seu numero: ");
+            try {
                 String digitado = TECLADO.readLine();
                 if(digitado.equals("sair")) {
                     break;
                 }
                 filaChamada.insere(new Chamada(digitado, listaDDDs));
                 filaChamada.printaFila();
+            } catch (Exception ex) {
+                System.err.println(ex.getMessage());
             }
-
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
         }
+    }
+
+    private static ListaDDDs populaListaDDD(Fila filaChamada) {
+        try {
+            ListaDDDs listaDDDs = JsonUtils.getListaDDD();
+            filaChamada.insere(new Chamada("9900000000", listaDDDs));
+            filaChamada.insere(new Chamada("34111111111", listaDDDs));
+            filaChamada.insere(new Chamada("3422222-2222", listaDDDs));
+            filaChamada.insere(new Chamada("(34)333333333", listaDDDs));
+            filaChamada.insere(new Chamada("(11)444444444", listaDDDs));
+            return listaDDDs;
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+        return null;
     }
 }
